fa9949a6f4aadd44694035b521a283454a4ad9ab
jeetshee4 <biswanathshee87@gmail.com>
2025-08-01 05:12:22 +0530

    PassSafe with MongoDB

--- src/component/Manager.jsx
+++ src/component/Manager.jsx
@@ -8,15 +8,22 @@ const Manager = () => {
     const [form, setform] = useState({ site: "", username: "", password: "" })
     const [passwordArray, setPasswordArray] = useState([])
 
+
+    const getPasswords = async()=>{
+        let req = await fetch("http://localhost:3000/")
+        let passwords = await req.json()
+        setPasswordArray(passwords)
+        
+        console.log(passwords)
+
+    }
+
+
+
+
     useEffect(() => {
-        let passwords = localStorage.getItem("passwords");
-        let passwordArray;
-        if (passwords) {
-            setPasswordArray(JSON.parse(passwords))
-        }
-        else {
-            passwordArray = []
-        }
+        getPasswords()
+        
     }, [])
 
 
@@ -32,29 +39,39 @@ const Manager = () => {
     }
 
 
-    const savePassword = () => {
+    const savePassword = async() => {
+
+        // if any such id exists in the database then delete it
+         await  fetch("http://localhost:3000", { method : "DELETE" , headers : { "Content-Type": "application/json" }, body: JSON.stringify({ id: form.id }) })   
+
+
 
         setPasswordArray([...passwordArray, {...form , id: uuidv4()}])
-        localStorage.setItem("passwords", JSON.stringify([...passwordArray, {...form , id: uuidv4()}]))
-        console.log(passwordArray)
+         await  fetch("http://localhost:3000", { method : "POST" , headers : { "Content-Type": "application/json" }, body: JSON.stringify({...form , id: uuidv4()}) })
+        
+        
+        // 
+        
+        // localStorage.setItem("passwords", JSON.stringify([...passwordArray, {...form , id: uuidv4()}]))
+        // console.log(passwordArray)
         setform({ site: "", username: "", password: "" })
     }
 
 
-    const handleDelete = (id) => {
+    const handleDelete = async(id) => {
          console.log("Deleting Password with id:" , id)
          let c = confirm("Do you really want to delete this password?")
          if(c) {
          setPasswordArray(passwordArray.filter(item=>item.id !== id))
-         localStorage.setItem("passwords", JSON.stringify(passwordArray.filter(item=>item.id !== id)))
-             
+        //  localStorage.setItem("passwords", JSON.stringify(passwordArray.filter(item=>item.id !== id)))
+          let res = await  fetch("http://localhost:3000", { method : "DELETE" , headers : { "Content-Type": "application/json" }, body: JSON.stringify({ id }) })   
          }
         // console.log(passwordArray)
     }
 
     const handleEdit = (id) => {
        console.log("Editing Password with id:" , id)
-       setform(passwordArray.filter(i=>i.id === id) [0])
+       setform({...passwordArray.filter(i=>i.id === id) [0] , id: id})
        setPasswordArray(passwordArray.filter(item=>item.id !== id))
 
     //    Here we will not be saving the password to localStorage immediately as 
@@ -104,7 +121,7 @@ const Manager = () => {
 
             />
             <div>
-                <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div></div>
+                <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div></div>
 
 
                 <div className="mx-auto mycontainer h-[85vh] overflow-auto">
@@ -145,7 +162,7 @@ const Manager = () => {
                     <div className="passwords">
                         <h2 className="font-bold">Your Passwords</h2>
                         {passwordArray.length === 0 && <div> No passwords to show </div>}
-                        {passwordArray.length != 0 && <table class="table-auto w-full rounded-xl overflow-hidden">
+                        {passwordArray.length != 0 && <table className="table-auto w-full rounded-xl overflow-hidden">
                             <thead className="bg-green-800 text-white">
                                 <tr>
                                     <th className="py-2">Site</th>
@@ -166,7 +183,7 @@ const Manager = () => {
                                                     </div>
 
 
-                                                    <div class="material-symbols-outlined hover:cursor-pointer " onClick={() => { copyText(item.site) }}>
+                                                    <div className="material-symbols-outlined hover:cursor-pointer " onClick={() => { copyText(item.site) }}>
                                                         content_copy
                                                     </div>
                                                 </div>
@@ -180,7 +197,7 @@ const Manager = () => {
 
                                                     </div>
 
-                                                    <div class="material-symbols-outlined hover:cursor-pointer " onClick={() => { copyText(item.username) }}>
+                                                    <div className="material-symbols-outlined hover:cursor-pointer " onClick={() => { copyText(item.username) }}>
                                                         content_copy
                                                     </div>
                                                 </div>
@@ -189,11 +206,11 @@ const Manager = () => {
                                             <td className="py-2 border border-white text-center">
                                                 <div className='flex justify-center items-center gap-2'>
                                                     <div>
-                                                        <a href={item.password} target='_blank'>{item.password}</a>
+                                                        <a href={item.password} target='_blank'>{"*".repeat(item.password.length)}</a>
 
 
                                                     </div>
-                                                    <div class="material-symbols-outlined hover:cursor-pointer " onClick={() => { copyText(item.password) }}>
+                                                    <div className="material-symbols-outlined hover:cursor-pointer " onClick={() => { copyText(item.password) }}>
                                                         content_copy
                                                     </div>
                                                 </div>
@@ -205,13 +222,13 @@ const Manager = () => {
                                                 <div className='flex justify-center items-center gap-2'>
                                                     <button  onClick={()=>{handleDelete(item.id)}} className="p-1 border border-green-800 flex justify-center items-center rounded-full hover:cursor-pointer mx-4 hover:bg-green-500">
                                                         Delete
-                                                        <div class="material-symbols-outlined">
+                                                        <div className="material-symbols-outlined">
                                                             delete
                                                         </div>
                                                     </button>
                                                      <button onClick={()=>{handleEdit(item.id)}} className="p-1 border border-green-800 flex justify-center items-center rounded-full hover:cursor-pointer mx-4 hover:bg-green-500">
                                                         Edit 
-                                                        <div class="material-symbols-outlined">
+                                                        <div className="material-symbols-outlined">
                                                             edit
                                                         </div>
                                                     </button>
